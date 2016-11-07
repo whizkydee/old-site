4046f10c42725272a3d32adc4c4e97f0178661da
Olaolu Olawuyi <whiskydee@gmail.com>
2016-11-07 21:00:13 +0100

    Add Gulpfile :neckbeard:

---
+++ gulpfile.babel.js
@@ -0,0 +1,63 @@
+import gulp         from 'gulp';
+import util         from 'gulp-util';
+import concat       from 'gulp-concat';
+import header       from 'gulp-header';
+import plumber      from 'gulp-plumber';
+import babel        from 'gulp-babel';
+import uglify       from 'gulp-uglify';
+import addsrc       from 'gulp-add-src';
+import autoprefixer from 'gulp-autoprefixer';
+import browserSync  from 'browser-sync';
+import child        from 'child_process';
+import hygienist    from 'hygienist-middleware';
+import del          from 'del';
+import fs           from 'fs';
+
+const parsed = JSON.parse(fs.readFileSync('./package.json'));
+const siteRoot = '_site';
+const jekyllLogger = buffer => {
+  buffer.toString().split(/\n/).forEach((message) => util.log(`Jekyll: ${message}`));
+};
+
+const paths = {
+  scripts: '_scripts/*.js',
+  dist: 'js/'
+};
+
+browserSync.create();
+
+gulp.task('clean', fn => del([paths.dist, siteRoot], fn));
+
+gulp.task('scripts', ['clean'], () => {
+  return gulp.src(paths.scripts)
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(addsrc.prepend(paths.libs))
+    .pipe(concat('bundle.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', ['serve', '--watch', '--incremental', '--drafts']);
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [`${siteRoot}/**`],
+    port: 2020,
+    server: {
+      baseDir: siteRoot,
+      middleware: hygienist(siteRoot)
+    }
+  });
+});
+
+
+gulp.task('watch', () => {
+  gulp.watch(paths.scripts, ['scripts']);
+});
+
+gulp.task('default', ['scripts', 'serve', 'jekyll', 'watch']);
