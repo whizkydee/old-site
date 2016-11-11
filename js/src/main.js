59699a5d7e4c80e7365232d319e96243927db7be
Olaolu Olawuyi <whiskydee@gmail.com>
2016-11-11 22:34:44 +0100

    Update scripts

---
+++ js/src/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+  const preloader = document.querySelector('.preloader');
+  setTimeout(() => {
+    preloader.classList.add('hide');
+  }, 900);
+
+}, false);
