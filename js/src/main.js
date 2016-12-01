b2fe87201414a45e44b24d07f00f670d0d3c2ca9
Olaolu Olawuyi <whiskydee@gmail.com>
2016-12-01 15:22:50 +0100

    Use ES5

--- js/src/main.js
+++ js/src/main.js
@@ -1,6 +1,7 @@
-window.addEventListener('load', () => {
-  const preloader = document.querySelector('.preloader');
-  setTimeout(() => {
+window.addEventListener('load', function() {
+  'use strict';
+  var preloader = document.querySelector('.preloader');
+  setTimeout(function() {
     preloader.classList.add('hide');
   }, 900);
 
